14588e08058127d3038539c82b823657bae499dc
BuildTools <unconfigured@null.spigotmc.org>
2022-06-04 22:18:35 -0400

    patched item entities and shulker boxes, all that's left is fucking entities

--- src/main/java/com/terriblefriends/itemshadowfixes/mixin/ShulkerBoxBlockMixin.java
+++ src/main/java/com/terriblefriends/itemshadowfixes/mixin/ShulkerBoxBlockMixin.java
@@ -1,20 +1,61 @@
 package com.terriblefriends.itemshadowfixes.mixin;
 
 import com.terriblefriends.itemshadowfixes.access.BlockEntityAccessor;
-import net.minecraft.block.ShulkerBoxBlock;
+import net.minecraft.block.*;
 import net.minecraft.block.entity.BlockEntity;
+import net.minecraft.block.entity.ShulkerBoxBlockEntity;
+import net.minecraft.entity.player.PlayerEntity;
 import net.minecraft.item.BlockItem;
+import net.minecraft.item.Item;
 import net.minecraft.item.ItemStack;
+import net.minecraft.loot.context.LootContext;
+import net.minecraft.loot.context.LootContextParameters;
+import net.minecraft.util.math.BlockPos;
+import net.minecraft.world.World;
+import org.jetbrains.annotations.Nullable;
+import org.lwjgl.system.CallbackI;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.Redirect;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
+
+import java.util.List;
 
 @Mixin(ShulkerBoxBlock.class)
-public class ShulkerBoxBlockMixin {
+public class ShulkerBoxBlockMixin extends BlockWithEntity {
+    ShulkerBoxBlock SBB_instance = (ShulkerBoxBlock) (Object) this;
+
+    public ShulkerBoxBlockMixin(Settings settings) {
+        super(settings);
+    }
+
     @Redirect(at=@At(value="INVOKE",target="Lnet/minecraft/block/entity/BlockEntity;setStackNbt(Lnet/minecraft/item/ItemStack;)V"),method="onBreak")
     public void setStackNbtDestroyShadows(BlockEntity instance, ItemStack stack) {
         System.out.println("nerd2");
         BlockItem.setBlockEntityNbt(stack, instance.getType(), ((BlockEntityAccessor)instance).createNbtShulkerDestroyShadows());
-        return;
+    }
+
+    @Inject(at=@At("HEAD"),method="Lnet/minecraft/block/ShulkerBoxBlock;getDroppedStacks(Lnet/minecraft/block/BlockState;Lnet/minecraft/loot/context/LootContext$Builder;)Ljava/util/List;",cancellable = true)
+    private void getDroppedStacksDestroyShadows(BlockState state, LootContext.Builder builder, CallbackInfoReturnable<List<ItemStack>> cir) {
+        BlockEntity blockEntity = builder.getNullable(LootContextParameters.BLOCK_ENTITY);
+        if (blockEntity instanceof ShulkerBoxBlockEntity) {
+            ShulkerBoxBlockEntity shulkerBoxBlockEntity = (ShulkerBoxBlockEntity)blockEntity;
+            builder = builder.putDrop(ShulkerBoxBlock.CONTENTS, (context, consumer) -> {
+                for(int i = 0; i < shulkerBoxBlockEntity.size(); ++i) {
+                    consumer.accept(shulkerBoxBlockEntity.getStack(i));
+                    shulkerBoxBlockEntity.getStack(i).setCount(0);
+                }
+
+            });
+        }
+
+        cir.setReturnValue(super.getDroppedStacks(state, builder));
+    }
+
+    @Nullable
+    public BlockEntity createBlockEntity(BlockPos pos, BlockState state) {
+        return new ShulkerBoxBlockEntity(SBB_instance.getColor(), pos, state);
     }
 }
